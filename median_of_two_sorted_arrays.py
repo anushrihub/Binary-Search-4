04c881094e64e32b15294e305f8e1bcb492ed230
Anushri <sastikaranushri@gmail.com>
2025-12-27 14:28:30 -0500

    logic correction

--- median_of_two_sorted_arrays.py
+++ median_of_two_sorted_arrays.py
@@ -22,7 +22,7 @@ class Solution:
             # L1 and L2 represents the largest value from the left side  and R1, R2 represents the smallest value from the right side
             L1 = float('-inf') if partX == 0 else nums1[partX - 1]
             R1 = float('inf') if partX == n1 else nums1[partX]
-            L2 = float('-inf') if partY == 0 else nums1[partY - 1]
+            L2 = float('-inf') if partY == 0 else nums2[partY - 1]
             R2 = float('inf') if partY == n2 else nums2[partY]
 
             # this condition checks if the partition is valid
